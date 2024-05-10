a9ffa2429d987dd81b9d655db10942b59918d33e
lokeshjangle <lokeshjangle2001@gmail.com>
2024-05-10 22:23:49 +0530

    second commit

---
+++ src/Route/index.js
@@ -0,0 +1,32 @@
+import  React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import FooterComponent from  '../Components/Footer';
+import HeaderComponent from '../Components/Header';
+import HomeContainer from '../Container/Home';
+import ContactContainer from '../Container/Contact';
+import DetailsContainer from '../Container/Details';
+import MoviesContainer from '../Container/Movies';
+import SearchContainer from '../Container/Search';
+import AboutContainer from '../Container/About';
+const  RouteComponent = ()=>{
+
+    return (
+        <>
+            <BrowserRouter>
+                <HeaderComponent />
+                    <Routes>
+                        <Route path="/" element={<HomeContainer />} />
+                        <Route path='/about' element={<AboutContainer />} />
+                        <Route path="/movies" element={<MoviesContainer />} />
+                        <Route path="/search" element={<SearchContainer />} />
+                        <Route path="/contact" element={<ContactContainer />} />
+                        <Route path="/details/:movieid/:mediatype" element={<DetailsContainer />} />
+                    </Routes>        
+                <FooterComponent />
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default RouteComponent;
